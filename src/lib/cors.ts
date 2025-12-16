bc15a74e9ff4cd29699bbbb535ae2d6494ae1dc8
pratik2542 <pratikmak2542@gmail.com>
2025-12-15 20:54:31 -0500

    Fix: Add X-User-Id to CORS allowed headers to fix analytics AI in APK

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -22,6 +22,6 @@ export function setCorsHeaders(res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-User-Id'
   )
 }
